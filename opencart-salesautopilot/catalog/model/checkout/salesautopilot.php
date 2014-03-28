1b3c4c36f698cb20011c6e3acccfe7ce2139f8a0
Khauth György <gykhauth@emesz.hu>
2014-03-28 17:41:00 +0100

    fix bugs
    Fix tax calculating
    Fix country code query

--- opencart-salesautopilot/catalog/model/checkout/salesautopilot.php
+++ opencart-salesautopilot/catalog/model/checkout/salesautopilot.php
@@ -7,8 +7,21 @@ class ModelCheckoutSalesAutopilot extends Model {
 			$items = array();
 			$tax = 0;
 		
+			$pcountry_query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "country` WHERE country_id = '" . $order_query->row['payment_country_id'] . "'");
+			if (!empty($pcountry_query->row)) {
+				$paymentISOCode = $pcountry_query->row['iso_code_2'];
+			} else {
+				$paymentISOCode = '';
+			}
+			$scountry_query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "country` WHERE country_id = '" . $order_query->row['shipping_country_id'] . "'");
+			if (!empty($scountry_query->row)) {
+				$shippingISOCode = $scountry_query->row['iso_code_2'];
+			} else {
+				$shippingISOCode = '';
+			}
+			
 			$product_query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "order_product` WHERE order_id = '" . (int)$order_id . "'");
-		
+					
 			foreach ($product_query->rows as $product) {
 				$category_query = $this->db->query("SELECT ptc.category_id, cd.name FROM `" . DB_PREFIX . "product_to_category` ptc LEFT JOIN `" . DB_PREFIX . "category_description` cd ON ptc.category_id = cd.category_id WHERE ptc.product_id = '" . (int)$product['product_id']  . "' LIMIT 1");
 			
@@ -20,8 +33,8 @@ class ModelCheckoutSalesAutopilot extends Model {
 					$category_name = 'No Category';
 				}
 			
-				if ($product['price'] > 0) {
-					$taxPercent = round($product['tax'] / $product['price']);
+				if (round($product['price'],2) > 0) {
+					$taxPercent = round(round($product['tax'],2) / round($product['price'],2) * 100,2);
 				} else {
 					$taxPercent = 0;
 				}
@@ -48,20 +61,20 @@ class ModelCheckoutSalesAutopilot extends Model {
 				'payment_method'	=> $order_query->row['payment_method'],
 				'currency'			=> $order_query->row['currency_code'],
 				'mssys_bill_company'	=> $order_query->row['payment_company'],
-				'mssys_bill_country'	=> strtolower($order_query->row['payment_iso_code_2']),
+				'mssys_bill_country'	=> strtolower($paymentISOCode),
 				'mssys_bill_state'		=> $order_query->row['payment_zone'],
 				'mssys_bill_zip'		=> $order_query->row['payment_postcode'],
 				'mssys_bill_city'		=> $order_query->row['payment_city'],
 				'mssys_bill_address'	=> $order_query->row['payment_address_1'].' '.$order_query->row['payment_address_2'],
 				'mssys_postal_company'	=> $order_query->row['shipping_company'],
-				'mssys_postal_country'	=> strtolower($order_query->row['shipping_iso_code_2']),
+				'mssys_postal_country'	=> strtolower($shippingISOCode),
 				'mssys_postal_state'		=> $order_query->row['shipping_zone'],
 				'mssys_postal_zip'		=> $order_query->row['shipping_postcode'],
 				'mssys_postal_city'		=> $order_query->row['shipping_city'],
 				'mssys_postal_address'	=> $order_query->row['shipping_address_1'].' '.$order_query->row['shipping_address_2'],
 				//'total'		 		=> round($order_query->row['total'],2),
 				'netshippingcost'	=> round($this->session->data['sap_shipping'],2),
-				'grossshippingcost'	=> round($this->session->data['sap_shipping'],2),
+				'grossshippingcost'	=> round($this->session->data['sap_shipping'] * (1 + $taxPercent / 100),2),
 				'products'	  => $items
 			);
 		}
